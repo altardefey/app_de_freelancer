8d7eb2a05d564375bf5fb66d3fd2d877eff57075
Rebeca Pena <crystalmeghan800@gmail.com>
2026-09-19 12:55:09 -0300

    Atualização do arquivo "supabase.ts"

--- freela_ggr/src/utils/supabase.ts
+++ freela_ggr/src/utils/supabase.ts
@@ -1,1 +1,6 @@
-// WIP - Em desenvolvimento
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://yckfxydpokyvizvbjout.supabase.co/rest/v1/';
+const supabaseKey = 'sb_publishable_ETA_Jjo0W-yCYpPoVDlu2w_XzEvYQ2V';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
